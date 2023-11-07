a3eae8541ecdf5a9d4588cbcb0a8d66b9de8f9c6
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-11-07 08:20:29 +0500

    list qaytar

--- basic_function_parameter01.py
+++ basic_function_parameter01.py
@@ -1,3 +1,7 @@
 # Create a function named get_data that takes no parameters.
 # Inside the function, define a list variable named data and assign it [1, 2, 3, 4, 5].
 # Return the data list.
+def get_data():
+    a=[1,2,3,4,5,6,7,8]
+    return a
+print(get_data())
