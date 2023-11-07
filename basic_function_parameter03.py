44d9898bf38c574c5288ad1aa5c65c34fe346c5d
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-11-07 09:15:31 +0500

    min ni topish

--- basic_function_parameter03.py
+++ basic_function_parameter03.py
@@ -1,3 +1,7 @@
 # Create a function named find_smallest that takes a list of numbers as a parameter.
 # Inside the function, find the smallest number in the given list.
 # Return the smallest number.
+def find_small(list):
+    a=min(list)
+    return a
+print(find_small([1,4,12,45]))
