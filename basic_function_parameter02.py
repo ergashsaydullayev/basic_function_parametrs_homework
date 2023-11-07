16b6cb321c4712292443d12b75ceb0da9e57b513
ergashsaydullayev <saydullayevergash600@gmail.com>
2023-11-07 09:11:01 +0500

    lit yigindisai

--- basic_function_parameter02.py
+++ basic_function_parameter02.py
@@ -1,3 +1,7 @@
 # Create a function named calculate_sum that takes a list of numbers as a parameter.
 # Inside the function, calculate the sum of all the numbers in the given list.
 # Return the sum.
+def number_sum(a):
+    s=sum(a)
+    return s
+print(number_sum([1,2,4,6,8,9,12]))
